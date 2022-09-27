fd6778d58b6d2af533d9ce29bcdfaa4e5140c12c
Matheus Rovero Rodrigues <vector.320@outlook.com>
2022-09-26 23:32:42 -0300

    Finalizado.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -18,7 +18,7 @@ export class Tab1Page {
   constructor(private alertController: AlertController) {}
 
   async alertaSoma() {
-
+    this.conta="Soma";
     this.resultado=(parseFloat(this.valor1) + parseFloat(this.valor2)).toString();
     const alert = await this.alertController.create({
       header: 'Resultado:',
@@ -33,6 +33,7 @@ export class Tab1Page {
   async alertaSubtracao() {
 
     this.resultado=(parseFloat(this.valor1) - parseFloat(this.valor2)).toString();
+    this.conta="Subtração";
     const alert = await this.alertController.create({
       header: 'Resultado:',
       message: `<strong>Valor 1: ${this.valor1} </strong><br>
@@ -41,11 +42,10 @@ export class Tab1Page {
       buttons: ['OK'],
     });
     await alert.present();
-    
   }
 
   async alertaDivisao() {
-
+    this.conta="Divisão";
     this.resultado=(parseFloat(this.valor1) / parseFloat(this.valor2)).toString();
     const alert = await this.alertController.create({
       header: 'Resultado:',
@@ -55,11 +55,10 @@ export class Tab1Page {
       buttons: ['OK'],
     });
     await alert.present();
-    
   }
 
   async alertaMultiplicacao() {
-
+    this.conta="Multiplicação";
     this.resultado=(parseFloat(this.valor1) * parseFloat(this.valor2)).toString();
     const alert = await this.alertController.create({
       header: 'Resultado:',
@@ -69,49 +68,52 @@ export class Tab1Page {
       buttons: ['OK'],
     });
     await alert.present();
-    
   }
-  
+
   calcularMenor(){
     if (parseFloat(this.valor1) < (parseFloat(this.valor2))){
-      this.resultado = this.valor1
+      this.resultado = `O menor número é ${this.valor1}`
     }else if (parseFloat(this.valor1) > (parseFloat(this.valor2))){
-      this.resultado = this.valor2
+      this.resultado = `O maior número é ${this.valor2}`
     }else if (parseFloat(this.valor1) == (parseFloat(this.valor2))){
       this.resultado="Ambos são iguais!"
     };
-
   }
 
   calcularMaior(){
     if (parseFloat(this.valor1) > (parseFloat(this.valor2))){
-      this.resultado = this.valor1
+      this.resultado = `O maior número é ${this.valor1}`
     }else if (parseFloat(this.valor1) < (parseFloat(this.valor2))){
-      this.resultado = this.valor2
+      this.resultado = `O maior número é ${this.valor2}`
     }else if (parseFloat(this.valor1) == (parseFloat(this.valor2))){
       this.resultado="Ambos são iguais!"
     };
-
   }
 
   async alertaMaior() {
     this.calcularMaior();
+    this.conta="Maior";
     const alert = await this.alertController.create({
       header: `${this.resultado}`
     });
     await alert.present();
-    
   }
 
   async alertaMenor() {
     this.calcularMenor();
+    this.conta="Menor";
     const alert = await this.alertController.create({
       header: `${this.resultado}`
     });
     await alert.present();
-    
   }
-  
+  async limpar() {
+    this.valor1="";
+    this.valor2="";
+    this.resultado="";
+    this.conta="";
+  }
+
 }
 
 
