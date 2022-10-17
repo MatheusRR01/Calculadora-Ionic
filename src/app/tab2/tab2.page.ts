df89ec147d23e4c5bcb5008a06dae1fb42c2709b
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2022-10-17 20:29:42 -0300

    Adicionado validação de caracteres

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab2',
@@ -8,29 +9,58 @@ import { Component } from '@angular/core';
 export class Tab2Page {
   operacao='';
   resultado='';
+  numero = false;
+  caracter = true;
+  caracteres = ['.', '/', '*', '+', '-'];
 
   constructor() {}
 
-  adicionarValor(valor: any){
-    if(valor==0 || valor== 1 || valor==2 || valor==3 || valor==4 || valor==5 || valor==6 || valor==7 || valor==8 || valor==9){
+  realizarOperacao(){
+    try{
+      this.resultado = evaluate(this.operacao);
+    }catch (err) {
+      this.resultado = 'Inválido!';
+    }
+    
+  }
+
+  adicionarValor(valor: string){
+    this.caracter = this.caracteres.includes(valor); // 'includes' é uma função do javascript que determina se um array inclue um certo elemento, retornando true ou false
+
+    if(!this.caracter) {
       this.operacao += valor;
-    }else if(this.operacao == '' || this.operacao != '*' || this.operacao != '/' || this.operacao != '+' || this.operacao != '-'){
-      this.operacao += valor; 
+      this.numero = true;
+    } else if (this.caracter && this.numero){
+      this.operacao += valor;
+      this.numero = false;
     }
   }
 
   limparC(){
     this.operacao ='';
     this.resultado ='';
+    this.numero = false;
   }
 
   limparCE(){
     this.operacao ='';
+    this.numero = false;
   }
 
   apagarCaracter(){
     if(this.operacao.length > 0){
       this.operacao = this.operacao.substring(0, this.operacao.length -1);
+    }
+
+    const ultimo = this.operacao.substring(this.operacao.length, 1); 
+    this.caracter = this.caracteres.includes(ultimo);
+
+    console.log(ultimo);
+
+    if(!this.caracter){
+      this.numero = true;
+    } else {
+      this.numero = false;
     }
   }
 
